a9b411eb0ee5ac47669e8bf2a39699c3ee8074ce
César O. Araújo <oaraujocesar@gmail.com>
2021-08-15 11:04:52 -0300

    chore: translating the help page

--- src/pages/help.js
+++ src/pages/help.js
@@ -8,6 +8,7 @@
 import React from 'react'
 import Layout from '@theme/Layout'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Translate, {translate} from '@docusaurus/Translate'
 import {GridBlock} from '../components/GridBlock'
 
 const ExternalLink = props => (
@@ -25,28 +26,47 @@ export default function Help(props) {
 
   const supportLinks = [
     {
-      content: `Ask a question on [Stack Overflow](https://stackoverflow.com/questions/tagged/react-testing-library)`,
+      content: translate({
+        message:
+          'Ask a question on [Stack Overflow](https://stackoverflow.com/questions/tagged/react-testing-library)',
+        id: 'helpStackOverFlow',
+      }),
       title: 'Stack Overflow',
     },
     {
-      content:
-        'Discuss issues with community members on [Spectrum](https://spectrum.chat/testing-library)',
+      content: translate({
+        message:
+          'Discuss issues with community members on [Spectrum](https://spectrum.chat/testing-library)',
+        id: 'helpSpectrum',
+      }),
       title: 'Spectrum',
     },
     {
-      content: `Chat on [Discord](https://discord.gg/testing-library)`,
+      content: translate({
+        message: 'Chat on [Discord](https://discord.gg/testing-library)',
+        id: 'helpDiscord',
+      }),
       title: 'Discord',
     },
     {
-      content: `Stay up to date by following the [blog](${props.config.baseUrl}blog)`,
+      content: translate({
+        message: 'Stay up to date by following the [blog](/blog)',
+        id: 'helpBlog',
+      }),
       title: 'Blog',
     },
     {
-      content: `Browse [Learning Material](/docs/learning)`,
-      title: 'Resources',
+      content: translate({
+        message: 'Browse [Learning Material](/docs/learning)',
+        id: 'helpResources',
+      }),
+      title: translate({message: 'Resources', id: 'helpResourcesTitle'}),
     },
     {
-      content: `Get support on [GitHub](${siteConfig.customFields.repoUrl})`,
+      content: translate({
+        message: `Get support on [GitHub]https://github.com/testing-library/dom-testing-library)`,
+        id: 'helpGitHub',
+      }),
       title: 'GitHub',
     },
   ]
@@ -61,7 +81,9 @@ export default function Help(props) {
         <div className="mainContainer documentContainer postContainer helpContainer">
           <div>
             <header>
-              <h1>Need help?</h1>
+              <h1>
+                <Translate id="helpTitle">Need help?</Translate>
+              </h1>
             </header>
             <GridBlock
               contents={supportLinks.slice(0, 3)}
@@ -74,13 +96,20 @@ export default function Help(props) {
               align="left"
             />
             <section>
-              <h2>Buy a Course</h2>
+              <h2>
+                <Translate id="helpBuyACourse">Buy a Course</Translate>
+              </h2>
               <p>
-                Learn how to test JavaScript with{' '}
+                <Translate id="helpCourseContentFirst">
+                  Learn how to test JavaScript with
+                </Translate>{' '}
                 <ExternalLink href="https://kentcdodds.com">
                   Kent C. Dodds
                 </ExternalLink>
-                , the creator of Testing Library, on{' '}
+                ,{' '}
+                <Translate id="helpCourseContentSecond">
+                  the creator of Testing Library, on
+                </Translate>{' '}
                 <ExternalLink href="https://testingjavascript.com">
                   TestingJavaScript.com
                 </ExternalLink>
@@ -89,36 +118,52 @@ export default function Help(props) {
                 <ExternalLink href="https://testingjavascript.com">
                   <img
                     width="500"
-                    alt="TestingJavaScript.com Learn the smart, efficient way to test any JavaScript application."
+                    alt={translate({
+                      message:
+                        'TestingJavaScript.com Learn the smart, efficient way to test any JavaScript application.',
+                      id: 'helpCourseAltText',
+                    })}
                     src={`${baseUrl}img/testingjavascript.jpg`}
                   />
                 </ExternalLink>
               </div>
             </section>
             <header className="postHeader">
-              <h1>Want to help?</h1>
+              <h1>
+                <Translate id="helpContribution">Want to help?</Translate>
+              </h1>
             </header>
             <p>
-              Thanks! The Testing Library maintainers are happy to maintain this
-              library along with you, the community. We aren't looking for
-              funding, but we need everyone to pitch in to make this project and
-              community successful and improve tests for everyone in the long
-              run.
+              <Translate id="helpContributionFirstParagraph">
+                Thanks! The Testing Library maintainers are happy to maintain
+                this library along with you, the community. We aren't looking
+                for funding, but we need everyone to pitch in to make this
+                project and community successful and improve tests for everyone
+                in the long run.
+              </Translate>
             </p>
             <p>
-              Please consider helping us answer community questions and update
-              documentation content via the help links above. You can also help
-              support{' '}
+              <Translate id="helpContributionSecondParagraph">
+                Please consider helping us answer community questions and update
+                documentation content via the help links above. You can also
+                help support
+              </Translate>{' '}
               <ExternalLink href="https://kentcdodds.com">
                 Kent C. Dodds
               </ExternalLink>{' '}
-              financially by purchasing his{' '}
+              <Translate id="helpContributionThirdParagraph">
+                financially by purchasing his
+              </Translate>{' '}
               <ExternalLink href="https://kentcdodds.com/courses">
-                courses
+                <Translate id="helpContributionFourthParagraph">
+                  courses
+                </Translate>
               </ExternalLink>{' '}
-              or{' '}
+              <Translate id="helpContributionFifthParagraph">or</Translate>{' '}
               <ExternalLink href="https://kentcdodds.com/workshops/">
-                remote workshops
+                <Translate id="helpContributionSixthParagraph">
+                  remote workshops
+                </Translate>
               </ExternalLink>
               .
             </p>
